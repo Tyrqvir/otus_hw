f16d9c821c3f1d4836cc934a4e6ba327ada48874
Vitaly <jqhelp@gmail.com>
2022-07-17 15:05:37 +0200

    feat: Done hw4

--- hw04_lru_cache/list_test.go
+++ hw04_lru_cache/list_test.go
@@ -15,6 +15,63 @@ func TestList(t *testing.T) {
 		require.Nil(t, l.Back())
 	})
 
+	t.Run("Start push from front", func(t *testing.T) {
+		l := NewList()
+
+		l.PushFront(1) // [1]
+		l.PushFront(2) // [2, 1]
+		l.PushBack(3)  // [2, 1, 3]
+		l.PushBack(4)  // [2, 1, 3, 4]
+
+		require.Equal(t, 4, l.Len())
+		require.Equal(t, 2, l.Front().Value)
+		require.Equal(t, 1, l.Front().Next.Value)
+		require.Equal(t, 3, l.Front().Next.Next.Value)
+		require.Equal(t, 4, l.Front().Next.Next.Next.Value)
+		require.Equal(t, 4, l.Back().Value)
+		require.Equal(t, 3, l.Back().Prev.Value)
+		require.Equal(t, 1, l.Back().Prev.Prev.Value)
+		require.Equal(t, 2, l.Back().Prev.Prev.Prev.Value)
+	})
+
+	t.Run("Start push from back", func(t *testing.T) {
+		l := NewList()
+
+		l.PushBack(1)  // [1]
+		l.PushBack(2)  // [1, 2]
+		l.PushFront(3) // [3, 1, 2]
+		l.PushFront(4) // [4, 3, 1, 2]
+
+		require.Equal(t, 4, l.Len())
+		require.Equal(t, 4, l.Front().Value)
+		require.Equal(t, 3, l.Front().Next.Value)
+		require.Equal(t, 1, l.Front().Next.Next.Value)
+		require.Equal(t, 2, l.Front().Next.Next.Next.Value)
+		require.Equal(t, 2, l.Back().Value)
+		require.Equal(t, 1, l.Back().Prev.Value)
+		require.Equal(t, 3, l.Back().Prev.Prev.Value)
+		require.Equal(t, 4, l.Back().Prev.Prev.Prev.Value)
+	})
+
+	t.Run("Remove exist item", func(t *testing.T) {
+		l := NewList()
+
+		l.PushFront(1) // [1]
+
+		require.Equal(t, 1, l.Len())
+
+		require.True(t, l.Remove(l.Front()))
+		require.Nil(t, l.Front())
+		require.Equal(t, 0, l.Len())
+		require.Nil(t, l.Front())
+		require.Nil(t, l.Back())
+	})
+
+	t.Run("Remove not exist item", func(t *testing.T) {
+		l := NewList()
+		require.False(t, l.Remove(l.Front()))
+	})
+
 	t.Run("complex", func(t *testing.T) {
 		l := NewList()
 
