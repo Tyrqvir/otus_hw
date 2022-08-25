2eb9d6494fb702e6d4cf0ffad3bb581104270a38
vitaly <jqhelp@gmail.com>
2022-08-25 08:15:06 +0200

    feat: Done HW07

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,67 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	size                     int64
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	source, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+
+	if offset != 0 {
+		source.Seek(offset, io.SeekStart)
+	}
+
+	defer source.Close()
+
+	sourceInfo, err := source.Stat()
+	if err != nil {
+		return err
+	}
+
+	size = sourceInfo.Size()
+
+	if offset > size {
+		return ErrOffsetExceedsFileSize
+	}
+	if size == 0 {
+		return ErrUnsupportedFile
+	}
+
+	destination, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+
+	defer destination.Close()
+
+	limit = func() int64 {
+		if limit == 0 || limit > size {
+			return size - offset
+		}
+		return limit
+	}()
+
+	bar := pb.Default.Start64(limit)
+	barReader := bar.NewProxyReader(source)
+
+	_, err = io.CopyN(destination, barReader, limit)
+	if err != nil {
+		return err
+	}
+
+	bar.Finish()
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,110 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	type args struct {
+		fromPath string
+		toPath   string
+		offset   int64
+		limit    int64
+	}
+
+	input := computedPathByName("input.txt")
+	tmpOutput, _ := os.CreateTemp("", "tests")
+	fmt.Println(tmpOutput.Name())
+
+	tests := []struct {
+		name    string
+		args    args
+		output  string
+		wantErr bool
+	}{
+		{
+			name: "Test with offset 0 and limit 0",
+			args: args{
+				fromPath: input,
+				toPath:   tmpOutput.Name(),
+				offset:   0,
+				limit:    0,
+			},
+			output:  computedPathByName("out_offset0_limit0.txt"),
+			wantErr: false,
+		},
+		{
+			name: "Test with offset 0 and limit 10",
+			args: args{
+				fromPath: input,
+				toPath:   tmpOutput.Name(),
+				offset:   0,
+				limit:    10,
+			},
+			output:  computedPathByName("out_offset0_limit10.txt"),
+			wantErr: false,
+		},
+		{
+			name: "Test with offset 0 and limit 1000",
+			args: args{
+				fromPath: input,
+				toPath:   tmpOutput.Name(),
+				offset:   0,
+				limit:    1000,
+			},
+			output:  computedPathByName("out_offset0_limit1000.txt"),
+			wantErr: false,
+		},
+		{
+			name: "Test with offset 0 and limit 10000",
+			args: args{
+				fromPath: input,
+				toPath:   tmpOutput.Name(),
+				offset:   0,
+				limit:    10000,
+			},
+			output:  computedPathByName("out_offset0_limit10000.txt"),
+			wantErr: false,
+		},
+		{
+			name: "Test with offset 100 and limit 1000",
+			args: args{
+				fromPath: input,
+				toPath:   tmpOutput.Name(),
+				offset:   100,
+				limit:    1000,
+			},
+			output:  computedPathByName("out_offset100_limit1000.txt"),
+			wantErr: false,
+		},
+		{
+			name: "Test with offset 6000 and limit 1000",
+			args: args{
+				fromPath: input,
+				toPath:   tmpOutput.Name(),
+				offset:   6000,
+				limit:    1000,
+			},
+			output:  computedPathByName("out_offset6000_limit1000.txt"),
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := Copy(tt.args.fromPath, tt.args.toPath, tt.args.offset, tt.args.limit); (err != nil) != tt.wantErr {
+				t.Errorf("Copy() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			sourceFileInfo, _ := os.Stat(tt.output)
+			targetFileInfo, _ := os.Stat(tt.args.toPath)
+			require.Equal(t, sourceFileInfo.Size(), targetFileInfo.Size())
+		})
+	}
+}
+
+func computedPathByName(name string) string {
+	return fmt.Sprintf("%s/%s", "testData", name)
 }
