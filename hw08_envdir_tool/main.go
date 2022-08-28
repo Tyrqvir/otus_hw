d227306244271295599e83c4d551a4e093572241
vitaly <jqhelp@gmail.com>
2022-08-28 09:21:37 +0200

    feat: Done HW08

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
+const (
+	argsCount      int = 2
+	defaultErrCode int = 1
+)
+
 func main() {
-	// Place your code here.
+	args := os.Args
+
+	if len(args) < argsCount {
+		fmt.Println("Invalid count of arguments, needed minimum ", argsCount)
+		os.Exit(defaultErrCode)
+	}
+
+	dirPath, cmdWithArgs := args[1], args[2:]
+
+	env, err := ReadDir(dirPath)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(defaultErrCode)
+	}
+
+	os.Exit(RunCmd(cmdWithArgs, env))
 }
